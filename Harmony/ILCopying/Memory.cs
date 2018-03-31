a2ee0321a83e3095b17660054d1e3a2648839d52
erdelf <erdelf.productions@gmail.com>
2018-03-31 16:10:03 +0200

    Added memory unprotecting from mainrepo

--- Harmony/ILCopying/Memory.cs
+++ Harmony/ILCopying/Memory.cs
@@ -1,14 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Harmony.ILCopying
 {
-	public static class Memory
-	{
-		public static long WriteJump(long memory, long destination)
+    [Flags]
+    public enum Protection
+    {
+#pragma warning disable IDE1006 // Benennungsstile
+        PAGE_NOACCESS = 0x01,
+        PAGE_READONLY = 0x02,
+        PAGE_READWRITE = 0x04,
+        PAGE_WRITECOPY = 0x08,
+        PAGE_EXECUTE = 0x10,
+        PAGE_EXECUTE_READ = 0x20,
+        PAGE_EXECUTE_READWRITE = 0x40,
+        PAGE_EXECUTE_WRITECOPY = 0x80,
+        PAGE_GUARD = 0x100,
+        PAGE_NOCACHE = 0x200,
+        PAGE_WRITECOMBINE = 0x400
+#pragma warning restore IDE1006 // Benennungsstile
+    }
+
+    public static class Memory
+    {
+        private static readonly HashSet<PlatformID> windowsPlatformIDSet = new HashSet<PlatformID>
+        {
+            PlatformID.Win32NT, PlatformID.Win32S, PlatformID.Win32Windows, PlatformID.WinCE
+        };
+
+        [DllImport("kernel32.dll")]
+ 	    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, Protection flNewProtect, out Protection lpflOldProtect);
+
+        public static bool IsWindows => windowsPlatformIDSet.Contains(Environment.OSVersion.Platform);
+
+        public static void UnprotectMemoryPage(long memory)
+        {
+            if (IsWindows)
+                if (!VirtualProtect(new IntPtr(memory), new UIntPtr(1), Protection.PAGE_EXECUTE_READWRITE, out Protection _ignored))
+                    throw new System.ComponentModel.Win32Exception();
+        }
+
+        public static long WriteJump(long memory, long destination)
 		{
+            UnprotectMemoryPage(memory);
+
 			if (IntPtr.Size == sizeof(long))
 			{
 				memory = WriteBytes(memory, new byte[] { 0x48, 0xB8 });
